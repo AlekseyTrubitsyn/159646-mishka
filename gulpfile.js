c9e89f877d16bf3844acd655187c7360543c02a2
Aleksey Trubtisyn <at.8441451@yandex.ru>
2016-10-09 02:00:47 +0300

    add svg sprites auto generation

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,13 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
     .pipe(plumber())
@@ -18,9 +25,15 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      /*mqpacker({
+        sort: true
+      })*/
     ]))
     .pipe(gulp.dest("css"))
+    /*.pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"))*/
     .pipe(server.stream());
 });
 
@@ -34,4 +47,14 @@ gulp.task("serve", ["style"], function() {
 
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img"));
 });
